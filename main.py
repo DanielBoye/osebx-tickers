850706c2fb99afadd319bd1096070b8592ff4659
Daniel Boye <danielboye888@gmail.com>
2023-01-27 09:57:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+tickers = ["AEGA", "AGLX", "AIRX", "AKAST", "AKBM", "ACH", "AKH", "AOW", "AKOBO", "ARCH", "ABS", "ARGEO", "ASTRO", "AURA", "ACR", "BGBIO", "BFISH", "CAPSL", "EAM", "ECIT", "ECO", "EWIND", "EIOF", "EMGS", "EFUEL", "EXTX", "FKRFT", "GEOS", "HAVI", "HYARD", "HDLY", "HUNT", "HYPRO", "KAHOT", "KOMPL", "KOA", "KYOTO", "LYTIX", "MVW", "MSEIS", "MRCEL", "MNTR", "NOC", "NEXT", "NORDH", "NANOV", "NUMND", "NODL", "NOL", "NAS", "NOR", "NYKD", "OBSRV", "PEXIP", "PGS", "QFUEL", "SCANA", "SKAND", "TEKNA", "VGM"]
+
+sentence = "https://finance.yahoo.com/quote/" 
+end = ".OL?p=&.tsrc=fin-srch"
+n = 0
+
+for word in tickers:
+    print(sentence + tickers[n] + end)
+    n = n + 1
