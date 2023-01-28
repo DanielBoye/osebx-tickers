79fd3e7b5a1327c0f035120012bf48e3f92de9e9
DanielBoye <danielboye888@gmail.com>
2023-01-28 23:21:55 +0100

    Update test.py
    Printing out when you've clicked on something

--- test.py
+++ test.py
@@ -4,20 +4,25 @@ import time
 
 # Open browser and navigate to URL
 driver = webdriver.Chrome()
-driver.get("https://finance.yahoo.com/quote/2020.OL?p=2020.OL&.tsrc=fin-srch")
+driver.get("https://finance.yahoo.com/quote/AEGA.OL?p=&.tsrc=fin-srch")
 
-time.sleep(2)
-
+driver.implicitly_wait(2)
+#maximize browser
+driver.maximize_window()
 # Locate the object that contains the number value
 # object = driver.find_element_by_xpath("/\//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[1]")
 
 cookies = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/div[2]/div[2]/button')
+print("Found cookies")
 cookies.click()
-time.sleep(3)
+print("Clicked cookies")
+driver.implicitly_wait(2)
 
 do_not_save = driver.find_element(By.XPATH, '//*[@id="myLightboxContainer"]/section/button[2]')
+print("Found no")
 do_not_save.click()
-time.sleep(3)
+print("Clicked no")
+driver.implicitly_wait(2)
 
 
 object = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[1]')
